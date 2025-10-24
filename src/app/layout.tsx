e4d874e6843d3268eace340a23d617114c6ff7b6
John Eric Sanchez <jsanchezsua@ensc.fr>
2025-10-24 21:09:38 +0200

    add jsonLd on head for google SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,34 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "John Eric Sanchez Suarez",
+    url: "https://joesanchezsu.github.io",
+    image: "https://joesanchezsu.github.io/images/avatar.jpg", // optional
+    jobTitle: "Creative Frontend Developer",
+    worksFor: {
+      "@type": "Organization",
+      name: "Independent / Freelance",
+    },
+    sameAs: [
+      "https://www.linkedin.com/in/john-eric-sánchez-suárez-b9b743158",
+      "https://x.com/jesanchezsua",
+      "https://github.com/joesanchezsu",
+    ],
+    description:
+      "Creative frontend developer and technologist with a background in mechatronics and cognitive engineering. Experienced in interactive web, 3D, and UX design.",
+  };
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Structured Data for Google */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body className={inter.variable}>
         <Providers>{children}</Providers>
       </body>
